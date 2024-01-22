743f01df53573f8f8322f0e0fa2abe8cb11d7732
ililihayy <lilipushkar15@gmail.com>
2024-01-22 16:56:07 +0200

    adding big maps

--- tictac_mvc.py
+++ tictac_mvc.py
@@ -1,24 +1,64 @@
 import os
+import math
 
 class Model:
-    def __init__(self, empty_cell_symbol = '-'):
+    def __init__(self, map_size = 3, empty_cell_symbol = '-'):
+        self.map_size = map_size
         self.map = []
-        for i in range(9):
+        for i in range(map_size * map_size):
             self.map.append(empty_cell_symbol)
 
 
     def CheckVictory(self, symbol):
-        if ((self.map[0] == symbol and self.map[1] == symbol and self.map[2] == symbol) or
-                (self.map[3] == symbol and self.map[4] == symbol and self.map[5] == symbol) or
-                (self.map[6] == symbol and self.map[7] == symbol and self.map[8] == symbol) or
-                (self.map[3] == symbol and self.map[0] == symbol and self.map[6] == symbol) or
-                (self.map[1] == symbol and self.map[4] == symbol and self.map[7] == symbol) or
-                (self.map[3] == symbol and self.map[5] == symbol and self.map[8] == symbol) or
-                (self.map[0] == symbol and self.map[4] == symbol and self.map[8] == symbol) or
-                (self.map[2] == symbol and self.map[4] == symbol and self.map[6] == symbol)):
+        for i in range(self.map_size):
+            row_match = True
+
+            for j in range(self.map_size):
+                if self.map[i * self.map_size + j] != symbol:
+                    row_match = False
+                    break
+            if row_match:
+                return True
+
+        
+        for i in range(self.map_size):
+            col_match = True
+            for j in range(self.map_size):
+                if self.map[j * self.map_size + i] != symbol:
+                    col_match = False
+                    break
+            if col_match:
+                return True
+            
+
+        diagonal1_match = True
+        for i in range(self.map_size):
+            if i < self.map_size and self.map[i * (self.map_size + 1)] != symbol:
+                diagonal1_match = False
+                break
+
+        diagonal2_match = True
+        for i in range(self.map_size):
+            if i < self.map_size and self.map[i * (self.map_size - 1) + (self.map_size - 1)] != symbol:
+                diagonal2_match = False
+                break
+
+        if diagonal1_match or diagonal2_match:
             return True
-        else:
-            return False
+
+
+        return False
+        # if ((self.map[0] == symbol and self.map[1] == symbol and self.map[2] == symbol) or
+        #         (self.map[3] == symbol and self.map[4] == symbol and self.map[5] == symbol) or
+        #         (self.map[6] == symbol and self.map[7] == symbol and self.map[8] == symbol) or
+        #         (self.map[3] == symbol and self.map[0] == symbol and self.map[6] == symbol) or
+        #         (self.map[1] == symbol and self.map[4] == symbol and self.map[7] == symbol) or
+        #         (self.map[3] == symbol and self.map[5] == symbol and self.map[8] == symbol) or
+        #         (self.map[0] == symbol and self.map[4] == symbol and self.map[8] == symbol) or
+        #         (self.map[2] == symbol and self.map[4] == symbol and self.map[6] == symbol)):
+        #     return True
+        # else:
+        #     return False
 
 
     def CheckForDraw(self, empty_cell_symbol):
@@ -34,10 +74,15 @@ class Model:
     
 class View:
     def PrintMap(self, map):
+        map_size = int(math.sqrt(len(map)))
+
         print(" Game board:")
-        print("|-" + map[0] + "-|-" + map[1] + "-|-" + map[2] + "-|")
-        print("|-" + map[3] + "-|-" + map[4] + "-|-" + map[5] + "-|")
-        print("|-" + map[6] + "-|-" + map[7] + "-|-" + map[8] + "-| \n")
+        for i in range(map_size):
+            row_str = "|"
+            for j in range(map_size):
+                row_str += "-" + map[i * map_size + j] + "-|"
+            print(row_str)
+        print()
 
     def AskingPlayersNames(self):
         print("\nHello, dear friends")
@@ -46,11 +91,19 @@ class View:
         print()
         return player1, player2
 
+    def AskingMapDetails(self):
+        print("\nPlease, enter map size. You can choose 3x3, 4x4, 5x5, 6x6, etc.")
+        map_size = int(input("We only use square maps, so write only one number, for example 4 "))
+        empty_cell_symbol = input("please, enter empty cell symbol ")
+        symbol1 = input("please, enter symbol1 ")
+        symbol2 =input("please, enter symbol2 ")
 
+        return map_size, empty_cell_symbol, symbol1, symbol2
+    
 class Controller:
-    def __init__(self, player1, player2, symbol1 = 'X', symbol2 = 'O', empty_cell_symbol = '-'):
+    def __init__(self, player1, player2, symbol1 = 'X', symbol2 = 'O', empty_cell_symbol = '-', map_size = 3):
         self.__v = View()
-        self.__m = Model(empty_cell_symbol)
+        self.__m = Model(3, empty_cell_symbol)
         self.player1 = player1
         self.player2 = player2
         self.empty_cell_symbol = empty_cell_symbol
@@ -118,5 +171,8 @@ class Controller:
 
 v = View()
 names = v.AskingPlayersNames()
-c = Controller(names[0], names[1])
+map_details = v.AskingMapDetails()
+print()
+
+c = Controller(names[0], names[1], map_details[2], map_details[3], map_details[1], map_details[0])
 c.PlayGame()
